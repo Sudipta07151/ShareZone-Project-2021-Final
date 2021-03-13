2de893e94d3e208ffef58ed59375f3814f5d19c5
Sudipta Adak <adak07151@gmail.com>
2021-03-14 00:17:51 +0530

    v1.11

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5500;
+app.use(express.static('./public'));
 app.get('/', (req, res) => {
-    res.send('HEY HEROKU APP STARTED')
+    res.sendFile(index.html);
 })
-
 app.listen(PORT);
