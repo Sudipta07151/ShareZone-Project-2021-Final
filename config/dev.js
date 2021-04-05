e7ac560e6ba276060483c55a58639f908c6248e0
Sudipta Adak <adak07151@gmail.com>
2021-04-06 01:32:54 +0530

    =

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '787874132753-eq5ueo744h9j6fcffra56jk8b1t1mlkd.apps.googleusercontent.com',
+    googleClientSecret: 'uHwZbnbp-CkuklDdfDbzIMRs',
+    mongoURI: 'mongodb+srv://sudipta:Sa11101997@cluster0.ixeou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'shwshankRedemption@1994'
+};
